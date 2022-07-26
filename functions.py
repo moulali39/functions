33492898bae22cdaf43696fa5851d90a397fb007
moulali39 <107177595+moulali39@users.noreply.github.com>
2022-07-26 12:51:17 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,63 @@
+# def functionname(name,age):
+#     print("this is ",name,age)
+
+# functionname("moulali",23)
+
+# def chemicalname(nature,name):
+#     print("this chemical is:",nature,name)
+# chemicalname("corrosive","iodine")   
+
+
+# def functionname(a,b):
+#     return(a*b)
+# w=functionname(3,5)   
+# print(w) 
+
+# def functionname(a,b):
+#     return(a,b)
+# c=functionname("moulali","hyderabad") 
+# print(c)   
+
+# def functionname(a,b):
+#     return(a-b)
+# w=functionname(25,3.5)   
+# print(w) 
+
+# def name():
+#     print("we are in hydetrabad")
+# name()    
+
+# def name(k):
+#     print("my name is" ,k)
+# name("moulali")
+
+# def name(fname,lname):
+#     print("my name is",fname,"lost name is",lname)
+# name("moulali","shaik")
+
+# def hii(*k):
+#     print("today our topic is:" , k)
+# hii("python")
+# hii("advanced python","core python")    
+
+# def hii(*c):
+#     print("company names in hyderabad:" ,c)
+# hii("divis","laurus","kcp","hetero")
+# hii("megha","ultratech")
+
+# def details(n,a):
+#     print('hey '+n+ ' ! your age is '+str(a))
+# details('hd',25)
+# # details(n='hd',a=26)
+# details(a=26,n='sd')
+
+
+# def address(a,b):
+#     print("hii moulali this is "+ a+ ' ! your city is '+ " just from km",str(b))
+# address("hyderabad",400)                                         
+
+def z(p,q):
+    return p*q,p+q;
+p=int(input()) 
+q=int(input())   
+print(z(p,q))
